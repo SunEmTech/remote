8f2482eed8febf6120f070a4ff9317801d72bd4d
Sandeep Kumar Verma <sunlights92@gmail.com>
2017-08-08 14:36:50 +0530

    mod: need to check

--- main.sh
+++ main.sh
@@ -1,1 +1,8 @@
 # from here actual execution should start
+
+main() {
+    if [1 -ne 0 ]; then
+        echo "runnig"
+        sleep 1
+    fi
+}
--- update.sh
+++ update.sh
@@ -1,1 +1,41 @@
-# Try
+# Pull and run
+PULL_DIR=$HOME/pull
+RUN_DIR=$HOME/run
+GIT_PATH=http://github.com/SunEmTech/remote
+
+pull() {
+
+    if ! [ -f $PULL_DIR/main.sh ]; then
+        git clone $GIT_PATH $PULL_DIR
+        return $?
+    else
+        git -C $PULL_DIR pull
+        return $?
+    fi 
+}
+
+update() {
+    cp $PULL_DIR/* $RUN_DIR
+}
+
+run() {
+    bash $RUN_DIR/main.sh
+}
+
+start() {
+    RETRY=3
+    if [ $RETRY -ne 0 ]; then
+        RETURN=`pull`
+        if [ $RETURN -ne 0 ]; then
+            echo "Check for the internet connection"
+            continue
+        else
+            break
+        fi
+        RETRY=`expr $RETRY - 1`
+    fi 
+    update  
+    run
+}
+
+start
